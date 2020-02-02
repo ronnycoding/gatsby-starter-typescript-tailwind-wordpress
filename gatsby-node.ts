0a948291eacca6d96bf6c189725b0f431c1be9a6
Ronny Freites <ronnyangelo.freites@gmail.com>
2020-02-01 21:10:18 -0600

    fix: ts support

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -4,7 +4,7 @@ exports.createPages = (({graphql, actions}) => {
   const { createPage } = actions
 
   return new Promise((resolve, reject) => {
-    const blogPostTemplate = path.resolve('src/templates/blogPost.js')
+    const blogPostTemplate = path.resolve('src/templates/blogPost.ts')
 
     resolve(
       graphql(
