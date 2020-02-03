762b822c2313a42b1c73faef72b9b60f02769065
Ronny Freites <ronnyangelo.freites@gmail.com>
2020-02-02 19:36:57 -0600

    more post css updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,12 @@ module.exports = {
     'gatsby-plugin-typescript',
     'gatsby-transformer-remark',
     {
+      resolve: 'gatsby-plugin-emotion',
+      options: {
+        // Accepts all options defined by `babel-plugin-emotion` plugin.
+      },
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'pages',
--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,15 @@
 /* eslint-disable no-unused-vars */
 const path = require('path')
 
+exports.onCreateWebpackConfig = ({actions, getConfig}) => {
+  // Hack due to Tailwind ^1.1.0 using `reduce-css-calc` which assumes node
+  // https://github.com/bradlc/babel-plugin-tailwind-components/issues/39#issuecomment-526892633
+  const config = getConfig()
+  config.node = {
+    fs: 'empty',
+  }
+}
+
 exports.createPages = ({graphql, actions}) => {
   const { createPage } = actions
 
